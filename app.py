e5723c96d40fceff8562e7fe73ac0e330923c131
David Ramos <davidramossal@gmail.com>
2024-10-24 03:10:39 +0300

    add the data extraction routine to first load of the page

--- app.py
+++ app.py
@@ -10,6 +10,51 @@ from sklearn.model_selection import TimeSeriesSplit
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import Lasso
 from sklearn.multioutput import MultiOutputRegressor
+from google.cloud import storage
+from google.oauth2 import service_account
+
+import os
+
+# Folder to check
+folder_name = "data"
+
+# Check if the folder exists
+if not os.path.exists(folder_name):
+
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcs_connections"]
+    )
+
+    # create a function to download a folder from google cloud bucket
+
+    def download_folder(bucket_name, prefix, local_folder, credentials):
+        client = storage.Client(credentials=credentials)
+
+        bucket = client.get_bucket(bucket_name)
+
+        blobs = bucket.list_blobs(prefix=prefix)
+
+        for blob in blobs:
+            relative_path = blob.name[len(prefix) :]
+
+            # If relative_path is empty or represents a directory, skip it
+            if not relative_path or relative_path.endswith("/"):
+                continue
+
+            # Create local destination path
+            local_path = os.path.join(local_folder, relative_path)
+
+            # Create directories if necessary
+            os.makedirs(os.path.dirname(local_path), exist_ok=True)
+
+            # Download the file
+            blob.download_to_filename(local_path)
+            print(f"Downloaded {blob.name} to {local_path}")
+
+    bucket_name = "predict-maintenanceproject"
+    prefix = "dashboard/"  # Folder in the bucket
+    local_folder = "data/dashboard/"  # Folder locally
+    download_folder(bucket_name, prefix, local_folder, credentials)
 
 # Set Streamlit page configuration
 st.set_page_config(
